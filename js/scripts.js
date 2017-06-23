1617f6b044758c1f8bc9759b37c407149e07eaf8
Faunie <faunie.szeloczei@gmail.com>
2017-06-22 20:56:51 -0700

    add unordered list and remove bullets

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,47 @@
 // business logic
 var pong = function(inputN) {
-   var result = [];
-   for (var i = 0; i <= inputN; i++ ) {
-     if (i % 3 === 0 && i % 15 !== 0) {
-       result.push("PING");
-     } else if (i % 5 === 0 && i % 15 !== 0) {
-       result.push("PONG");
-     } else if (i % 15 === 0) {
-       result.push("PING-PONG");
-     } else {
-       result.push(i);
-     }
- }
- return result;
+  var result = [];
+  for (var i = 0; i <= inputN; i++) {
+    if (i % 3 === 0 && i % 15 !== 0) {
+      result.push("PING");
+    } else if (i % 5 === 0 && i % 15 !== 0) {
+      result.push("PONG");
+    } else if (i % 15 === 0) {
+      result.push("PING-PONG");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
 };
 
 // jQuery user interface logic here.
 $(function() {
- $(".pong" ).submit(function(event) {
-   event.preventDefault();
-   $("ul#showOutput").empty();
-   var inputN = parseInt($("#inputNumber").val());
-   var output = pong(inputN);
-   for (var i = 1; i <= inputN; i++) {
-    $("#showOutput").append("<li>" + output[i] + "</li>");
-  }
+  $(".pong").submit(function(event) {
+    event.preventDefault();
+    $("ul#showOutput").empty();
+    var inputN = parseInt($("#inputNumber").val());
+    var output = pong(inputN);
+    for (var i = 1; i <= inputN; i++) {
+      $("#showOutput").append("<li>" + output[i] + "</li>");
+    }
   });
- });
+});
 
- function myFunction() {
-     document.getElementById("myDropdown").classList.toggle("show");
- }
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
- window.onclick = function(event) {
-   if (!event.target.matches('.dropbtn')) {
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
 
-     var dropdowns = document.getElementsByClassName("dropdown-content");
-     var i;
-     for (i = 0; i < dropdowns.length; i++) {
-       var openDropdown = dropdowns[i];
-       if (openDropdown.classList.contains('show')) {
-         openDropdown.classList.remove('show');
-       }
-     }
-   }
- }
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
