1f5209da951e30c077441fe34d467fa5c059d5aa
Faunie <faunie.szeloczei@gmail.com>
2017-06-22 21:11:45 -0700

    refactors dropdown javascript function

--- js/scripts.js
+++ js/scripts.js
@@ -26,22 +26,8 @@ $(function() {
       $("#showOutput").append("<li>" + output[i] + "</li>");
     }
   });
+
+  $("#about-the-creator").click(function() {
+    $("#links").toggle();
+  });
 });
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
